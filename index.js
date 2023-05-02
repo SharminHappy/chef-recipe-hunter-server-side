d4580a81c52688ae0d667cf2809b1b6b0e54ac1d
sharmin akter <sharmin.happy.25@gmail.com>
2023-05-03 02:30:31 +0600

    chef recipe sever succesfully connected with client server ViewchafeRecipe  page

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port= 5000;
 
 
 const chefsInfo=require('./data/chefsInfo.json');
+const viewChefRecipes=require('./data/chefsInfo.json');
+
+
+
 
 
 app.use(cors());
@@ -17,6 +21,24 @@ app.get('/',(req,res)=>{
 
 app.get('/chefsInfo',(req,res)=>{
     res.send(chefsInfo);
+})
+
+// app.get('/chefInfo/:id',(req,res)=>{
+//     const id=req.params.id;
+//     console.log(id);
+//     const selectedChefInfo=chefsInfo.find(n=>n.id==id);;
+//     res.send(selectedChefInfo)
+// })
+
+app.get('/viewChefRecipes',(req,res)=>{
+    res.send(viewChefRecipes);
+})
+
+app.get('/viewChefRecipes/:id',(req,res)=>{
+    const id=req.params.id;
+    const selectedViewChefsRecipes= viewChefRecipes.find(n=>n.id ==id);
+    res.send(selectedViewChefsRecipes)
+
 })
 
 app.listen(port,()=>{
