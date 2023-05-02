08667a311514da1b2bd7b656bcaf4b0185f7e474
sharmin akter <sharmin.happy.25@gmail.com>
2023-05-02 21:17:17 +0600

    chef information data created and also server connected with client side

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express=require('express');
 const app=express();
+const cors=require('cors')
 const port= 5000;
+
+
+const chefsInfo=require('./data/chefsInfo.json');
+
+
+app.use(cors());
 
 
 app.get('/',(req,res)=>{
     res.send('Chef Recipe is cooking')
 
 });
+
+app.get('/chefsInfo',(req,res)=>{
+    res.send(chefsInfo);
+})
 
 app.listen(port,()=>{
     console.log(`chef recipe cooking is running:${port}`)
