43dd1d6dfe8035d1e0893cb9b587593aa728916e
sharmin akter <sharmin.happy.25@gmail.com>
2023-05-03 05:22:26 +0600

    all recipes data created

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ app.get('/chefsInfo',(req,res)=>{
     res.send(chefsInfo);
 })
 
-// app.get('/chefInfo/:id',(req,res)=>{
-//     const id=req.params.id;
-//     console.log(id);
-//     const selectedChefInfo=chefsInfo.find(n=>n.id==id);;
-//     res.send(selectedChefInfo)
-// })
+app.get('/chefInfo/:id',(req,res)=>{
+    const id=req.params.id;
+    console.log(id);
+    const selectedChefInfo=chefsInfo.find(n=>n.id == id);;
+    res.send(selectedChefInfo)
+})
 
 app.get('/viewChefRecipes',(req,res)=>{
     res.send(viewChefRecipes);
